2322daee0e620edd86c45d7fd1a851ab9aaa5961
Nasir Waqas <nasirwaqas744@gmail.com>
2025-05-29 11:28:36 +0500

    commit-routes

--- server/server.js
+++ server/server.js
@@ -1,54 +1,7 @@
-// const express = require('express');
-// const colors = require('colors');
-// const morgan = require('morgan');
-// const dotenv = require('dotenv');
-// const { connectDB } = require('./config/db');
-// const cors = require('cors');
 
-// //dotenv config
-// dotenv.config();
-
-// // connect to database
-// connectDB();
-
-// // rest object
-// const app = express();
-// app.use(cors());
-// //middlevares
-// app.use(morgan('dev'));
-// app.use(express.json());
-
-// //routes
-// app.use("/user", require("./routes/userRoutes"));
-// app.use("/pet", require("./routes/petRoute"));
-
-
-// app.get('/', (req, res) => {
-//   res.status(200).send({
-//     message: "server is running"
-//   });
-// });
-// // port
-// const port = process.env.PORT || 8080;
-// // listen port
-// app.listen(port, () => {
-//   console.log(`Server is running in ${process.env.NODE_MODE} mode on port ${process.env.PORT}`.yellow.bold);
-// });
-
-// // Serve frontend
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../client/build")));
-
-//   app.get("*", (req, res) =>
-//     res.sendFile(
-//       path.resolve(__dirname, "../", "client", "build", "index.html")
-//     )
-//   );
-// } else {
-//   app.get("/", (req, res) => res.send("Please set to production"));
-// }
 const express = require('express');
 const colors = require('colors');
+
 const morgan = require('morgan');
 const dotenv = require('dotenv');
 const path = require('path');
@@ -73,9 +26,9 @@ app.use(express.json());
 app.use('/images', express.static(path.join(__dirname, 'public/images')));
 
 // Routes
-app.use("/user", require("./routes/userRoute"));
-app.use("/pet", require("./routes/petRoute"));
-app.use("/admin", require("./routes/adminroute"));
+// app.use("/user", require("./routes/userRoute"));
+// app.use("/pet", require("./routes/petRoute"));
+// app.use("/admin", require("./routes/adminroute"));
 
 // Health check route
 app.get('/', (req, res) => {
