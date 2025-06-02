bcc7c12a6d525a3f96049c34f30d17600374d0bf
Nasir Waqas <nasirwaqas744@gmail.com>
2025-06-02 14:44:59 +0500

    undo modifications

--- client/src/index.js
+++ client/src/index.js
@@ -9,6 +9,8 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
 root.render(
   <Provider store={store}> 
   <React.StrictMode>
