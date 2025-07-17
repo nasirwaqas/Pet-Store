29df40f307b6923178bf425378284880057d828b
Nasir Waqas <nasirwaqas744@gmail.com>
2025-07-17 08:23:04 +0200

    db-conenction change

--- server/config/db.js
+++ server/config/db.js
@@ -3,10 +3,36 @@ const colors = require('colors');
 
 const connectDB = async () => {
     try {
-      await mongoose.connect(process.env.MONGO_URL);
-      console.log(`Mongodb server is connected to ${mongoose.connection.host}`.bgGreen.white.bold);
+        // Set mongoose global options
+        mongoose.set('bufferCommands', false);
+
+        mongoose.set('strictQuery', false);
+        
+        const conn = await mongoose.connect(process.env.MONGO_URL, {
+            maxPoolSize: 10,
+            serverSelectionTimeoutMS: 5000,
+            socketTimeoutMS: 45000,
+        });
+        
+        console.log(`MongoDB Connected: ${conn.connection.host}`.bgGreen.white.bold);
+        
+        // Handle connection events
+        mongoose.connection.on('error', (err) => {
+            console.log(`MongoDB connection error: ${err}`.bgRed.white);
+        });
+        
+        mongoose.connection.on('disconnected', () => {
+            console.log('MongoDB disconnected'.bgYellow.black);
+        });
+        
+        mongoose.connection.on('reconnected', () => {
+            console.log('MongoDB reconnected'.bgGreen.white);
+        });
+        
     } catch (error) {
-      console.log(`Mongodb server Issue: ${error.message}`.bgRed.white);
+        console.log(`MongoDB server Issue: ${error.message}`.bgRed.white);
+        process.exit(1);
     }
 };
+
 module.exports = { connectDB };
